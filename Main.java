ea112426f1b93fe97f42bba2235e2eeb54ba4c28
JANANI-SUBASHINI <janani.jananir.r@gmail.com>
2017-07-07 10:23:54 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,21 @@
+# subu
+import java.io.*;
+import java.util.*;
+public class Main456
+{
+  public static void main(String args[])  
+  {
+    Scanner sc=new Scanner(System.in);
+    int a,b,num;
+  System.out.println("Enter the num");
+  a=sc.nextInt();
+  System.out.println("Enter the value of b");
+  b=sc.nextInt();
+    for(int i=1;i<=12;i++)
+  {
+    num=a*i;
+    
+    System.out.println(a+"*"+b+"="+num);
+  }
+}
+}  
